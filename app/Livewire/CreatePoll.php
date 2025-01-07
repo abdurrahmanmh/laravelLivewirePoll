b849c86c7379de6cb2fbafdcf9e65e955ece6b3f
Abdurrahman <arahmanmh15@gmail.com>
2025-01-07 18:56:18 +0800

    10 realtime validation

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -3,12 +3,27 @@
 namespace App\Livewire;
 
 use App\Models\Poll;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CreatePoll extends Component
 {
+    #[Validate]
     public $title;
-    public $options = [];
+    #[Validate]
+    public $options = ['First'];
+
+    protected $rules = [
+        'title' => 'required|min:3|max:255',
+        'options' => 'required|array|min:1|max:255',
+        'options.*' => 'required|min:1|max:255'
+    ];
+
+    protected $messages =[
+        'options.*.required' => 'Option cannot be empty'
+    ];
+
+    
     public function render()
     {
         return view('livewire.create-poll');
@@ -16,6 +31,7 @@ class CreatePoll extends Component
 
     public function addOption(){
         $this->options[] = '';
+        $this->validateOnly('options');
     }
 
     public function removeOption($index){
@@ -23,7 +39,14 @@ class CreatePoll extends Component
         $this->options = array_values($this->options);
     }
 
+    // public function updated($propertyName){
+    //     $this->validateOnly($propertyName);
+    // }
+
     public function createPoll(){
+        $this->validate();
+
+
         Poll::create([
             'title' => $this->title
         ])->options()->createMany(
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -2,6 +2,9 @@
     <form wire:submit.prevent="createPoll">
         <label>Poll Title</label>
         <input type="text" wire:model.live="title" />
+        @error('title')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
 
         <div class="mb-4">
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
@@ -14,9 +17,11 @@
 
                     <div class="flex gap-2">
                         <input type="text" wire:model="options.{{ $index }}" />
-                        <button class="btn" wire:click.prevent="removeOption({{$index}})">Remove</button>
+                        <button class="btn" wire:click.prevent="removeOption({{ $index }})">Remove</button>
                     </div>
-
+                    @error("options.{$index}")
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             @endforeach
         </div>
