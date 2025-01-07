44a149861225e1d951d4de824089f2bea49556f9
Abdurrahman <arahmanmh15@gmail.com>
2025-01-07 16:20:41 +0800

    07 edit poll option

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -5,13 +5,18 @@
         Current title: {{ $title }}
 
         <div class="mb-4">
-          <button class="btn" wire:click.prevent="addOption">Add Option</button> 
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
         </div>
 
-        <div >
+        <div>
             @foreach ($options as $index => $option)
                 <div class="mb-4">
-                  {{ $index }} - {{ $option }}
+                    <label>Option {{ $index + 1 }}</label>
+
+                    <div class="flex gap-2">
+                        <input type="text" wire:model="options.{{ $index }}" />
+                        <button class="btn" wire:click.prevent="removeOption({{$index}})">Remove</button>
+                    </div>
 
                 </div>
             @endforeach
